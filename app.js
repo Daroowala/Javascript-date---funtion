001705109a32530bc5feffc6b6ea87d9e4d7cd11
my name <blueshaperd3@gmail.com>
2024-04-25 00:56:42 +0500

    changing condition is ok

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-// chat
-for (var a = 1; a <= 100; a = a+10){
-for (var b = a; b < a+10; b++){
-    document.write(b + " ")
-}
-document.write("<br>")
+// table
+// for (var a = 1; a <= 100; a = a+10){
+// for (var b = a; b < a+10; b++){
+//     document.write(b + " ")
+// }
+// document.write("<br>")
+// }
+
+// changingCase
+// var city = prompt("Enter Your City Name");
+// city =city.toLowerCase()
+// var arr = ["karachi", "lahore", "islamabad", "quetta"]
+
+// for (var i = 0; i < arr.length; i++){
+//     if(arr[i] === city){
+//         alert("City is found")
+//         break;
+//     }
+//     else{
+//         alert("City is not found")
+//     }
+// }
+    
+var city = prompt("Enter Your City Name");
+city = city.toLowerCase();
+var arr = ["karachi", "lahore", "islamabad", "quetta"];
+var cityFound = false;
+
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === city) {
+        cityFound = true;
+        break;
+    }
 }
 
+if (cityFound) {
+    alert("City is found");
+} else {
+    alert("City Not Found");
+}
+
+
+
+// date
 //  date = new Date();
 //  year = date.getFullYear();
 //  day = date.getDay();
