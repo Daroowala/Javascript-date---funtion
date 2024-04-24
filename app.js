50d581e95ddb6af5bfc412284d4cdedeaaae4374
my name <blueshaperd3@gmail.com>
2024-04-24 22:57:08 +0500

    start dates

---
+++ app.js
@@ -0,0 +1,12 @@
+var date = new Date();
+var year = date.getFullYear();
+var day = date.getDay();
+var hour = date.getHours();
+var month = date.getMonth();
+var second = date.getSeconds();
+document.write(date)
+document.write(year)
+document.write(day)
+document.write(hour)
+document.write(month)
+document.write(second)
