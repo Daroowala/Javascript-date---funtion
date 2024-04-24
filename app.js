56987b1acd2c13ec22a910e1e6ae49623ed319d5
my name <blueshaperd3@gmail.com>
2024-04-25 00:06:24 +0500

    table complete

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var date = new Date();
-var year = date.getFullYear();
-var day = date.getDay();
-var hour = date.getHours();
-var month = date.getMonth();
-var second = date.getSeconds();
-document.write(date)
-document.write(year)
-document.write(day)
-document.write(hour)
-document.write(month)
-document.write(second)
+// chat
+for (var a = 1; a <= 100; a = a+10){
+for (var b = a; b < a+10; b++){
+    document.write(b + " ")
+}
+document.write("<br>")
+}
+
+//  date = new Date();
+//  year = date.getFullYear();
+//  day = date.getDay();
+//  hour = date.getHours();
+//  month = date.getMonth();
+//  second = date.getSeconds();
+// document.write(date)
+// document.write(year)
+// document.write(day)
+// document.write(hour)
+// document.write(month)
+// document.write(second)
